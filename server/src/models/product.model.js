3b2d87a59c35b096e05d16f2fc49596aef0bc16f
AmitParihar17 <amitparihar1208@gmail.com>
2026-02-24 23:36:27 +0530

    add product model

---
+++ server/src/models/product.model.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    image: {
+      type: [String],
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    subCategory: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    bestseller: {
+      type: Boolean,
+      default: false,
+    },
+    sizes: {
+      type: [String],
+      enum: ["S", "M", "L", "XL"],
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export const Product = mongoose.model("Product", productSchema);
